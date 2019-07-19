78cdb16e3d0997d52334ed26bd10ad8d4002bd41
Kenya-a <Kenya-a@users.noreply.github.com>
2019-07-19 11:31:26 -0500

    trying to map

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -2,28 +2,34 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
 
+const user = function () { }
+
 function Characters() {
     const [data, setData] = useState([]);
-useEffect(() => {
-    axios.get("https://swapi.co/api/people/1")
+    // const [type, setType] = useState('name');
+    useEffect(() => {
+        axios.get(`https://swapi.co/api/people/1`)
 
-    .then(response => {
-        console.log('StarWars! :', response)
-        setData(response.data);
-    })
+            .then(response => {
+                console.log('StarWars! :', response)
+                setData(response.data);
+            })
 
-    .catch(error => {
-        console.log('The Empire:', error)
-    })
-    
-},[])
+            .catch(error => {
+                console.log('The Empire:', error)
+            })
+
+    }, [])
 
 
-    return(
-        <div>Hello
+    return (
+        <div>Hello,
             {data.name}
+            {object.values(array).map(name =>{
+                key = {name}
+            })}
         </div>
-        
+
     )
 };
 
